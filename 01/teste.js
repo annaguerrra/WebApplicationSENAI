77ede47acf8fcf310e14331282cde0cc237676f0
annaguerrra <annaguerracastro@gmail.com>
2025-08-05 10:31:29 -0300

    more things

--- 01/teste.js
+++ 01/teste.js
@@ -2,3 +2,4 @@ console.log("Hello, World!");
 
 console.log("we're hungry!");
 
+console.log("This repository is also used for training version control practices, including commits, branching, and collaborative workflows with Git.");
