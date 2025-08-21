7729432aa5dbcbfc83e2fec32fb705a7defd38da
annaguerrra <annaguerracastro@gmail.com>
2025-08-21 11:19:27 -0300

    check task done

--- 04 WebBosch/script.js
+++ 04 WebBosch/script.js
@@ -1,29 +1,29 @@
 const AddTask = () => {
-
     const inputText = document.getElementById('input').value.trim();
-    if(inputText == '') return;
-        
-    const li = document.createElement('li');
+    if (inputText === '') return;
 
+    const li = document.createElement('li');
     li.textContent = inputText;
 
     const taskList = document.getElementById('tasklist');
     taskList.appendChild(li);
 
-    document.getElementById('input').value='';
+    document.getElementById('input').value = '';
 
     let check = document.createElement('check');
-    check.id = "check"
-    check.style.backgroundColor = "rgba(101, 101, 102, 0.363);" 
-    check.style.color = "rgb(129, 199, 25);";
+    check.id = "check";
+
+    check.textContent = "✔";
+    check.style.cursor = "pointer";
+    check.style.display = "flex";
     check.style.fontSize = "20px";
-    check.style.display = "flex"
-    check.style.alignItems = "center" 
-    check.onclick = ;
+    check.style.marginLeft = "10px";
+    check.style.color = "rgb(129, 199, 25)";
 
-}
+    check.onclick = () => {
+        li.classList.toggle("checked");
+    };
 
-const DeleteTask = () => {
-    
-    const 
-}
+    li.appendChild(check);
+
+};
