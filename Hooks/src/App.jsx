93abe695f89b9e2189ffc00d61f50f27e9dcdbe1
annaguerrra <annaguerracastro@gmail.com>
2025-09-03 08:54:36 -0300

    correcting the visibility button

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -2,32 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Hidden } from './Components/Hidden'
 
 function App() {
   const [count, setCount] = useState(0)
   const [text , setText] = useState("")
-  const [hidden, setHidden] = useState(false)
+  const [hidden, setHidden] = useState(false) // setar invisibilidade
   const [change, setChange] = useState(false)
 
   return (
     <>
-    <div className='ChangeColor'>
-      <button 
-        onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-        </button>
-
-        <input type="text" onChange={(e) => setText(e.target.value)}/>
-
-      <h1 className={hidden ? "hide" : "show"}>{text}</h1>
-
-      <button  onClick={() => setHidden(!hidden)}>Change Letter Color</button>
-      
-        <h1 className={change ? "change" : "redo"}>Changing</h1>
-        <button onClick={() => setChange(!change)}>Change div Color</button>
+    {hidden && 
+      <div>
+        <h1>It's here</h1>
       </div>
+    }
 
+    <button onClick={() => {setHidden(hidden == true ? false : true)}}>
+      Change Visibility
+    </button>
     </>
   )
 }
