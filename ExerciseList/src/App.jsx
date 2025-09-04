ba89ac0ee4ad24c4f091299d9ff683ce77ac3677
annaguerrra <annaguerracastro@gmail.com>
2025-09-04 11:31:30 -0300

    dois clocks essa merda deu erro

---
+++ ExerciseList/src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import './App.css'; // apenas CSS
+
+function App() {
+  const [count, setCount] = useState(0);
+  
+  
+  
+  return (
+    
+  )
+}
+
+export default App
