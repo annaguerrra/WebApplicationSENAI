ffbd8ff0a713bf21ca1a7f6295bc295bafa574d6
annaguerrra <annaguerracastro@gmail.com>
2025-09-04 11:42:40 -0300

    starting counter

--- ExerciseList/src/App.jsx
+++ ExerciseList/src/App.jsx
@@ -4,10 +4,21 @@ import './App.css'; // apenas CSS
 function App() {
   const [count, setCount] = useState(0);
   
-  
+  const increase = setCount((prev) => prev + 1);
+  const decrease = setCount((prev) => prev - 1);
   
   return (
-    
+    <div
+    style={{ 
+      backgroundColor: "gray",
+      color: "white",
+      borderRadius: "10px",
+      padding: "20px",
+      textAlign: "center",
+      width: "300px",
+      margin: "50px auto"}}>
+      <h1>Counter</h1>
+    </div>
   )
 }
 
