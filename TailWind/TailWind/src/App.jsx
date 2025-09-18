a1d5aa50ffd9d8eaff900e81f72dc3307bb4f365
annaguerrra <annaguerracastro@gmail.com>
2025-09-18 11:38:45 -0300

    quadrados linha 2 started

--- TailWind/TailWind/src/App.jsx
+++ TailWind/TailWind/src/App.jsx
@@ -4,11 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='flex justify-between items-center'>
+      <div className='flex justify-between items-center mb-20'>
         <div className='w-40 h-40 bg-blue-500 flex flex-col items-center justify-center gap-14'>
           <div className='w-13 h-13 bg-yellow-400 '></div>
           <div className='w-13 h-13 bg-yellow-400 '></div>
@@ -26,7 +25,12 @@ function App() {
         <div className='w-40 h-40 bg-blue-500 flex items-center justify-center'>
           <div className='w-13 h-13 bg-yellow-400 self-end'></div>
         </div>
+      </div>
 
+      <div className='flex justify-between items-center mb-20'>
+        <div className='w-40 h-40 bg-blue-500 flex items-center justify-center'>
+          <div className='w-13 h-13 bg-yellow-400 self-start'></div>
+        </div>
       </div>
         
 
