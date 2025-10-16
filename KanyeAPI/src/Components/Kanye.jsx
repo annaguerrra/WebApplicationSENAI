84bfb0816d6c6a65c0084286a54c5459263defea
annaguerrra <annaguerracastro@gmail.com>
2025-10-15 21:46:51 -0300

    feito, parcialmente

--- KanyeAPI/src/Components/Kanye.jsx
+++ KanyeAPI/src/Components/Kanye.jsx
@@ -1,25 +1,47 @@
 import { useEffect, useState } from "react"; 
 import axios from "axios"; // HTTP requests
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
+
+import kanye01 from "../assets/kanye01.jpg"; 
+import kanye02 from "../assets/kanye02.jpg"; 
+import kanye03 from "../assets/kanye03.jpg";
+import kanye04 from "../assets/kanye04.jpg";
+import kanye05 from "../assets/kanye05.jpg";  
+import kanye06 from "../assets/kanye06.jpg";
+import kanye07 from "../assets/kanye07.jpg"; 
+import kanye08 from "../assets/kanye08.jpg";
+import kanye09 from "../assets/kanye09.jpg";
+import kanye10 from "../assets/kanye10.jpg"; 
+import kanye11 from "../assets/kanye11.jpg";
+import kanye12 from "../assets/kanye12.jpg";
+import kanye13 from "../assets/kanye13.jpg"; 
+import kanye14 from "../assets/kanye14.jpg";
+import kanye15 from "../assets/kanye15.jpg"; 
+
 
 export default function Kanye() {
-  const [quote, setQuote] = useState(""); 
-  const navigate = useNavigate(); 
+    const listImages = [
+        kanye01, kanye02, kanye03, kanye04, kanye05, kanye06, kanye07, kanye08,
+        kanye09, kanye10, kanye11, kanye12, kanye13, kanye14, kanye15];
+    const [quote, setQuote] = useState(""); 
+    const [current, setCurrent] = useState(0);
+    const navigate = useNavigate(); 
 
   
-  useEffect(() => {
-    getQuote(); 
-  }, []);
+    useEffect(() => {
+        getQuote(); 
+    }, []);
 
   
-  const getQuote = async () => {
-    try {
-      const response = await axios.get("https://api.kanye.rest/"); 
-      setQuote(response.data.quote); 
-    } catch (error) {
-      console.error("Error fetching quote:", error); 
-    }
-  };
+    const getQuote = async () => {
+        try {
+            const response = await axios.get("https://api.kanye.rest/"); 
+            setQuote(response.data.quote); 
+            setCurrent((prev) => (prev + 1) % listImages.length)} 
+        catch (error) {
+            console.error("Error fetching quote:", error); 
+        }
+    };
 
   
   const goAbout = () => {
@@ -29,9 +51,10 @@ export default function Kanye() {
   return (
     <div className="flex flex-col items-center mt-10">
       
-      <h1 className="text-black font-serif italic text-2xl text-center">{quote}</h1>
-      <h3 style={{ fontFamily: 'cursive' }}> - Kanye West</h3>
+      <img src={listImages[current]} alt="ye" width={"300px"}/>
 
+      <h1 className="text-black font-serif italic text-2xl text-center">{quote}</h1>
+      <h3 style={{ fontFamily: 'italic', fontSize: '40px'}}> - Kanye West</h3>
       
       <button
         onClick={getQuote}
@@ -48,6 +71,7 @@ export default function Kanye() {
         Go to About
       </button>
     </div>
+    
   );
 }
 
